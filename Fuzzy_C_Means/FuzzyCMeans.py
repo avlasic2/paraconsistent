7994207d530c09c0d3a4c9daebccc8015b509a9b
avlasic2 <45577336+avlasic2@users.noreply.github.com>
2022-02-20 09:51:45 -0600

    Added KL

--- Fuzzy_C_Means/FuzzyCMeans.py
+++ Fuzzy_C_Means/FuzzyCMeans.py
@@ -2,24 +2,51 @@ import numpy as np
 from tqdm import tqdm
 
 class FuzzyCMeans():
-    def __init__(self,data,numberClusters):
-        self.data = data
+    def __init__(self, data, numberClusters, useKLDiv = False ):
         self.numberClusters = numberClusters
+        self.useKLDiv = useKLDiv
+
+        if useKLDiv:
+            self.data = self.maptoSimplex( data )
+        else:
+            self.data = data
+
         self.clusterCenters = np.random.normal( 0., 1. , (numberClusters, data.shape[1]) )
-        self.weights = np.random.weibull( 5., ( data.shape[0] , numberClusters ) )
+        if useKLDiv:
+            self.clusterCenters = self.maptoSimplex( self.clusterCenters )
+
+        unnormalizedWeights = np.random.pareto( 5., ( data.shape[0] , numberClusters ) )
+        self.weights = unnormalizedWeights/unnormalizedWeights.sum(1).reshape(-1,1)
+
+        if useKLDiv:
+            self.dist = self._dataCenterDistKL
+        else:
+            self.dist = self._dataCenterDistLP
+
+
+    def maptoSimplex(self, arr):
+        expMap = np.hstack( ( np.exp(arr), np.ones( ( arr.shape[0] , 1  ) ) ) )
+        return expMap/expMap.sum(1).reshape(-1,1)
+
 
     def updateCenters(self, power):
         weightsCalc = ( self.weights.copy() )**power
         self.clusterCenters = np.dot( ( weightsCalc / weightsCalc.sum(0) ).T, self.data )
 
-    def updateWeights(self, power):
-        EucDist = ( self._dataCenterDist( self.data, self.clusterCenters) )**( 2./ (power-1.) )
-        self.weights = (1./( np.einsum('ij,ik->jik', EucDist, EucDist**(-1)) ).sum(2) ).T
 
-    def _dataCenterDist(self, ar1, ar2, lp = 2.):
+    def updateWeights(self, power, lp):
+        dist = ( self.dist( self.data, self.clusterCenters, lp) )**( 2./ (power-1.) )
+        self.weights = (1./( np.einsum('ij,ik->jik', dist, dist**(-1)) ).sum(2) ).T
+
+
+    def _dataCenterDistLP(self, ar1, ar2, lp = 2.):
         return ( ( ( ar1[:, np.newaxis] - ar2 )**lp ).sum(2) )**(1./lp)
 
-    def train(self, power = 2., epsilon = .01, runs = 1000):
+
+    def _dataCenterDistKL(self, ar1, ar2, lp=2.):
+        return (-1.) * ( np.array([ar2] * ar1.shape[0]) * np.log(ar1[:, np.newaxis] * ar2 ** (-1)) ).sum(2)
+
+    def train(self, power = 2., lp = 2.,  epsilon = .01, runs = 1000):
         previousWeights = self.weights.copy()
 
         err = 2*epsilon
@@ -28,7 +55,7 @@ class FuzzyCMeans():
         iter = 0
 
         while (err > epsilon) and ( iter < runs+1 ):
-            self.updateWeights(power=power)
+            self.updateWeights(power=power, lp=lp)
             self.updateCenters(power=power)
             err = np.abs( previousWeights - self.weights ).max()
             previousWeights = self.weights.copy()
@@ -37,7 +64,9 @@ class FuzzyCMeans():
             pbar.set_postfix(Train_loss=err)
         pbar.close()
 
+
     def softPredict(self,Indices=None):
         if Indices == None:
             return self.weights.argmax(1)
         return self.weights[ Indices ].argmax(1)
+
--- Fuzzy_C_Means/TestClustering.py
+++ Fuzzy_C_Means/TestClustering.py
@@ -6,15 +6,17 @@ r2 = np.random.normal(-2., 1., (500,10) )
 
 data = np.vstack( (r1, r2) )
 
-model = FuzzyCMeans(data,2)
+model = FuzzyCMeans(data ,numberClusters = 2, useKLDiv = True)
 
-model.train(power = 2.1, epsilon = .001, runs = 1000)
+model.train(power = 2.1, lp = 2., epsilon = .0001, runs = 1000)
 
 #select 5 data points from each distribution
 indices = [0,1,2,3,4,500,501,502,503,504]
 
 #display data
-print( data[ indices ] )
+print( 'Data points:\n', data[ indices ] )
 
 #display the soft predicted group each data point was calculated to be with
-print( model.softPredict( indices ) )
+print( 'Predicted cluster for each data point:', model.softPredict( indices ) )
+
+print('Weights for each data points:\n', model.weights[ indices ])
